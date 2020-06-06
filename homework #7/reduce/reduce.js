f647529edba7cfb15b97ca7fb8be81f9eb23557c
Elida Maruqyan <elida.marukyan@gmail.com>
2020-06-07 03:19:13 +0400

    add homework #7 solutions

--- homework #7/reduce/reduce.js
+++ homework #7/reduce/reduce.js
@@ -10,10 +10,3 @@ reduce([1, 2, 3, 4, 5], (result, item) => {
     result.push(item * 2);
     return result;
 }, []); // [ 2, 4, 6, 8, 10 ]
-
-
-const cb = (result, item) => {
-    result.push(item * 2);
-    return result;
-};
-reduce([1, 2, 3, 4, 5], cb, []); // [ 2, 4, 6, 8, 10 ]
