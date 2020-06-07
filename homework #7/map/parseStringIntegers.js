30c9d1145bd21e64f19163c0e60e0e8e7300d8f5
Elida Maruqyan <elida.marukyan@gmail.com>
2020-06-07 11:43:51 +0400

    add homework #7 solutions

--- homework #7/map/parseStringIntegers.js
+++ homework #7/map/parseStringIntegers.js
@@ -1,10 +1,10 @@
 //old
-// const parseInteger = (values) => {
-//     return values.map((item) => Number.isNaN(parseInt(item)) ? null : parseInt(item));
-// };
-//
-// parseInteger(["1", "2", "34"]); // [1, 2, 34];
-// parseInteger(["1", "px", "2323"]); // [1, null, 2323];
+const parseInteger = (values) => {
+    return values.map((item) => Number.isNaN(parseInt(item)) ? null : parseInt(item));
+};
+
+parseInteger(["1", "2", "34"]); // [1, 2, 34];
+parseInteger(["1", "px", "2323"]); // [1, null, 2323];
 
 // new
 const parseInteger = (args) => {
