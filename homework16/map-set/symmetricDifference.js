ad3dda258b00d4c757abfa9f288e0bc78325b8c9
Elida <35566567+elidamaruqyan@users.noreply.github.com>
2020-07-30 22:59:49 +0400

    Update symmetricDifference.js

--- homework16/map-set/symmetricDifference.js
+++ homework16/map-set/symmetricDifference.js
@@ -1,17 +1,16 @@
 let setA = new Set([1, 2, 3, 4]);
 let setC = new Set([3, 4, 5, 6]);
 
-const symmetricDifference = (setA, setB) => {
-    const _symmetricDifference = new Set([...setA, ...setB]);
+const getSymmetricDifference = (setA, setB) => {
+  const _symmetricDifference = new Set([...setA, ...setB]);
 
-    for (let item of setA.values()) {
-        if (setB.has(item)) {
-            _symmetricDifference.delete(item);
-        }
+  for (let item of setA.values()) {
+    if (setB.has(item)) {
+      _symmetricDifference.delete(item);
     }
+  }
 
-    return _symmetricDifference;
+  return _symmetricDifference;
 };
 
-symmetricDifference(setA, setC); // => Set [1, 2, 5, 6]
-
+getSymmetricDifference(setA, setC); // => Set [1, 2, 5, 6]
