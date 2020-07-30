365c0737090c3029bc9e408a3f3fc2ed10ffa2bf
Elida <35566567+elidamaruqyan@users.noreply.github.com>
2020-07-30 23:04:33 +0400

    Update getOneDigit.js

--- homework16/recursion/getOneDigit.js
+++ homework16/recursion/getOneDigit.js
@@ -4,24 +4,23 @@
  */
 
 const getOneDigit = (num) => {
-    let stringifiedNumber = num.toString().split("");
+  let stringifiedNumber = num.toString().split("");
 
-    let sum = stringifiedNumber.reduce((acc, item) => {
-        acc += parseInt(item);
-        console.log(acc);
-        return acc;
-    }, 0);
-    return sum >= 10 ? getOneDigit(sum) : sum;
+  let sum = stringifiedNumber.reduce((acc, item) => {
+    acc += parseInt(item);
+    console.log(acc);
+    return acc;
+  }, 0);
+  return sum >= 10 ? getOneDigit(sum) : sum;
 };
 
 getOneDigit(14); //5
 getOneDigit(29); //2
 getOneDigit(999999999999); //9
 
-
 // algorithm to get one digit
 function digital_root(n) {
-    return (n - 1) % 9 + 1;
+  return ((n - 1) % 9) + 1;
 }
 
 // digital_root(14);
