263204508f9263972787940cdfbcd9520c5a2842
Elida <35566567+elidamaruqyan@users.noreply.github.com>
2020-07-30 23:04:03 +0400

    Update flattenArr.js

--- homework16/recursion/flattenArr.js
+++ homework16/recursion/flattenArr.js
@@ -3,12 +3,11 @@
  *@param {Array}  arr of nested arrays.
  */
 
-
-const flattenArr = (arr, res=[]) => {
-    arr.forEach((item) => (
-        Array.isArray(item)) ? flattenArr(item, res) : res.push(item)
-    );
-    return res
+const flattenArr = (arr, res = []) => {
+  arr.forEach((item) =>
+    Array.isArray(item) ? flattenArr(item, res) : res.push(item)
+  );
+  return res;
 };
 
 console.log(flattenArr([1, [3, 4, [1, 2]], 10])); //[1, 3, 4, 1, 2, 10]
