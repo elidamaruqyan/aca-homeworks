ace454be8a58148a49ec2e4cc8b0fb3eef55528a
Elida <35566567+elidamaruqyan@users.noreply.github.com>
2020-07-30 22:54:44 +0400

    Update index.js

--- homework17/formData/index.js
+++ homework17/formData/index.js
@@ -1,31 +1,29 @@
-const formData = document.querySelector('#form-data');
-const submitForm = document.querySelector('#get-info');
-const result = document.querySelector('#result');
+const formData = document.querySelector("#form-data");
+const submitForm = document.querySelector("#get-info");
+const result = document.querySelector("#result");
 const inputs = formData.elements;
 const values = {};
 
-
 const formSerialize = () => {
-    for (let i = 0; i < inputs.length - 1; i++) {
-        if (inputs[i].value.trim() === "") {
-            inputs[i].classList.add("error")
-        }else{
-            inputs[i].classList.remove("error");
-        }
-        if (inputs[i].type === "radio") {
-            if(inputs[i].checked) {
-                values[inputs[i].name] = inputs[i].value;
-            }
-        } else {
-            values[inputs[i].name] = inputs[i].value;
-        }
-
+  for (let i = 0; i < inputs.length - 1; i++) {
+    if (inputs[i].value.trim() === "") {
+      inputs[i].classList.add("error");
+    } else {
+      inputs[i].classList.remove("error");
     }
-    return values;
+    if (inputs[i].type === "radio") {
+      if (inputs[i].checked) {
+        values[inputs[i].name] = inputs[i].value;
+      }
+    } else {
+      values[inputs[i].name] = inputs[i].value;
+    }
+  }
+  return values;
 };
 
 submitForm.addEventListener("click", (e) => {
-    e.preventDefault();
-    const response = formSerialize(formData);
-    result.innerText = JSON.stringify(response);
+  e.preventDefault();
+  const response = formSerialize(formData);
+  result.innerText = JSON.stringify(response);
 });
