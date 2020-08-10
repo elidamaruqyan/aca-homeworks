ab01304636bb66b314eb59dda9ab700fc408aa0f
Elida Maruqyan <elida.marukyan@gmail.com>
2020-08-10 18:03:17 +0400

    added to-do lists item

--- homework18/searchBooks/main.js
+++ homework18/searchBooks/main.js
@@ -7,7 +7,8 @@ const searchedResult = document.querySelector("#booksWrapper");
 
 let url;
 const state = {
-    inputValue: ""
+    inputValue: "",
+
 };
 
 searchBtn.addEventListener("click", (e) => {
@@ -24,22 +25,31 @@ searchBtn.addEventListener("click", (e) => {
 const getBook = (url) => {
     doGet(url)
         .then((data) => {
-            data.docs.forEach((elem) => {
+            console.log(data);
+            console.log(url);
+            const list = document.createElement("ul");
+            data.docs.forEach((book) => {
                 const listItem = document.createElement("li"),
                       title = document.createElement("h2"),
                       authorName = document.createElement("h5"),
-                      firstPublishYear = document.createElement("h4");
+                      firstPublishYear = document.createElement("h4"),
+                      subject = document.createElement("p");
 
-                title.innerText = "Title: " + elem.title;
-                authorName.innerText = "Author Name: " + elem.author_name;
-                firstPublishYear.innerText = "First Publish Year: " + elem.first_publish_year;
+                title.innerText = "Title: " + book.title;
+                authorName.innerText = "Author Name: " + book.author_name;
+                firstPublishYear.innerText = "First Publish Year: " + book.first_publish_year;
+                subject.innerText = "Subject: " + book.subject;
 
                 listItem.append(title, authorName, firstPublishYear);
 
-
-                searchedResult.innerHTML = "";
-                searchedResult.appendChild(listItem);
-                console.log(searchedResult)
+                list.appendChild(listItem);
             });
+            searchedResult.innerHTML = "";
+            searchedResult.append(list)
         });
+};
+
+
+const handlePagination = () => {
+
 };
